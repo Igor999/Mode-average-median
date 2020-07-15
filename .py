134e447c7e39b67b226b1c685aa5f1a0be15fe15
Igor Semenov <semenow.igorek@yandex.ru>
2020-07-15 14:59:13 +0300

    Update2.py
    Added comparison

--- .py
+++ .py
@@ -1,10 +1,22 @@
 ar = input().split()
-ar = [int(i) for i in ar]
+ar = [float(i) for i in ar]
 ar = sorted(ar)
 n = []
 m = []
 c = 0
 d = 0
+res = 0
+Me = 0
+for i in ar:
+    res += i
+Sr = round((res/len(ar)),1)
+print("Среднее: "+str(Sr))
+if len(ar)%2 != 0:
+    Me = round(ar[len(ar)//2],1)
+    print("Медиана:",str(Me))
+else:
+    Me = round(((ar[int(len(ar)/2)])+(ar[int(len(ar)/2-1)]))/2,1)
+    print("Медиана:",str(Me))    
 for i in ar:
     if ar.count(i) >= c:
         c = ar.count(i)
@@ -13,15 +25,44 @@ for i in ar:
         if ar.count(i) not in m:
             m.append(ar.count(i))
 m = max(m)
+mo = []
 for i in n:
     if ar.count(i) == m:
-        print(str(i), end=" ")
-print("\n")
-res = 0
-for i in ar:
-    res += i
-print("Среднее"+str(res/len(ar)))
-if len(ar)%2 != 0:
-    print("Медиана:",str(round(ar[len(ar)//2],1)))
+        mo.append(i)
+MOD = ""
+if len(mo)==1:
+    MOD += "Мода: "+str(int(mo[0]))
 else:
-    print("Медиана:",str(round((ar[int(len(ar)/2)])+(ar[int(len(ar)/2-1)])/2,1)))  
+    MOD = "Моды: "
+    for i in mo:
+        MOD += str(int(i))+" "
+    MOD = MOD[:-1]
+print(MOD)
+ar_res = []
+for i in mo:
+    if i == Me:
+        if "Мо=Ме" not in ar_res:
+            ar_res.append("Мо=Ме")
+    elif i < Me:
+        if "Мо<Ме" not in ar_res:
+            ar_res.append("Мо<Ме")
+    elif i > Me:
+        if "Мо>Ме" not in ar_res:
+            ar_res.append("Мо>Ме")
+    if i == Sr:
+        if "Мо=Ср" not in ar_res:
+            ar_res.append("Мо=Ср")
+    elif i < Sr:
+        if "Мо<Ср" not in ar_res:
+            ar_res.append("Мо<Ср")
+    elif i > Sr:
+        if "Мо>Ср" not in ar_res:
+            ar_res.append("Мо>Ср")
+if Me==Sr:
+    ar_res.append("Ме=Ср")
+elif Me<Sr:
+    ar_res.append("Ме<Ср")
+elif Me>Sr:
+    ar_res.append("Ме>Ср")
+for i in ar_res:
+    print(i)
